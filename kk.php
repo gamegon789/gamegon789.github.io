65ee8e4f1b8d407a841690378acdc84aba3e14df
GameGon <130536765+gamegon789@users.noreply.github.com>
2023-04-27 21:38:52 +0600

    Delete kk.php

--- kk.php
+++
@@ -1,5 +0,0 @@
- echo '<div class="form_message form_success_messagee">
-              <a href="https://ellcizin.github.io/" class="close-popup3">&times;</a>
-              <h3>Sent Successfuly</h3>
-              <p class="p_color">We will reply as <span class="s_success">Soon</span>  as posible</p>
-            </div>'
